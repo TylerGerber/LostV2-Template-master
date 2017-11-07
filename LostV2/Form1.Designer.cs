eea7f6fca7417b6fa6d024e66e5842b42c5c508f
tyler gerber <tygerber12@gmail.com>
2017-11-07 16:06:03 -0500

    Add project files.

---
+++ LostV2/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace LostV2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.redLabel = new System.Windows.Forms.Label();
+            this.blueLabel = new System.Windows.Forms.Label();
+            this.greenlabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.ForeColor = System.Drawing.Color.White;
+            this.outputLabel.Location = new System.Drawing.Point(24, 28);
+            this.outputLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(339, 257);
+            this.outputLabel.TabIndex = 2;
+            this.outputLabel.Text = "label1";
+            // 
+            // redLabel
+            // 
+            this.redLabel.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.redLabel.ForeColor = System.Drawing.Color.White;
+            this.redLabel.Location = new System.Drawing.Point(67, 313);
+            this.redLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.redLabel.Name = "redLabel";
+            this.redLabel.Size = new System.Drawing.Size(296, 23);
+            this.redLabel.TabIndex = 3;
+            this.redLabel.Text = "label1";
+            // 
+            // blueLabel
+            // 
+            this.blueLabel.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.blueLabel.ForeColor = System.Drawing.Color.White;
+            this.blueLabel.Location = new System.Drawing.Point(67, 346);
+            this.blueLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.blueLabel.Name = "blueLabel";
+            this.blueLabel.Size = new System.Drawing.Size(296, 23);
+            this.blueLabel.TabIndex = 4;
+            this.blueLabel.Text = "label1";
+            // 
+            // greenlabel
+            // 
+            this.greenlabel.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.greenlabel.ForeColor = System.Drawing.Color.White;
+            this.greenlabel.Location = new System.Drawing.Point(67, 381);
+            this.greenlabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.greenlabel.Name = "greenlabel";
+            this.greenlabel.Size = new System.Drawing.Size(296, 23);
+            this.greenlabel.TabIndex = 5;
+            this.greenlabel.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(386, 422);
+            this.Controls.Add(this.greenlabel);
+            this.Controls.Add(this.blueLabel);
+            this.Controls.Add(this.redLabel);
+            this.Controls.Add(this.outputLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Clint Eastwod";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Label redLabel;
+        private System.Windows.Forms.Label blueLabel;
+        private System.Windows.Forms.Label greenlabel;
+    }
+}
+
---
+++ LostV2/Form1.cs
@@ -0,0 +1,139 @@
+﻿/// created by : 
+/// date       : 
+/// description: A basic text adventure game engine
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LostV2
+{
+    public partial class Form1 : Form
+    {
+        // tracks what part of the game the user is at
+        int scene = 0;
+
+        // random number generator
+        Random randGen = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            outputLabel.Text = "Year: 1870 \nPlace: Frontier \n \nYou" +
+                         " are working on the farm with you papa and hear a b" +
+                         "ig noise in the distance";
+            redLabel.Text = "Tell him";
+            blueLabel.Text = "Leave it";
+            greenlabel.Text = "";
+
+            //display initial message and options
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            /// check to see what button has been pressed and advance
+            /// to the next appropriate scene
+            if (e.KeyCode == Keys.M)       //red button press
+            {
+                if (scene == 0) { scene = 1; }
+                else if (scene == 1) { scene = 3; }
+                if (scene == 3) { scene = 4; }
+                if (scene == 4) { scene = 5; }
+
+                
+
+                }
+                else if (e.KeyCode == Keys.B)  //blue button press
+                {
+                    if (scene == 0) { scene = 2; }
+                    if (scene == 4) { scene = 5; }
+                    if (scene == 3) { scene = 4; }
+                }
+                else if (e.KeyCode == Keys.C)  //green
+                {
+
+                }
+
+                /// Display text and game options to screen based on the current scene
+                switch (scene)
+                {
+                    case 0:  //start scene 
+                        outputLabel.Text = "Year: 1870 /n Place: Frontier /n /n You" +
+                            " are working on the farm with you papa and hear a b" +
+                            "ig noise in the distance";
+                        redLabel.Text = "Tell him";
+                        blueLabel.Text = "Leave it";
+                        break;
+                    case 1:
+                        outputLabel.Text = "He also hears it but before he can react " +
+                            "bandits attack your farm! They shoot papa and all the cows. " +
+                            "You notice a scar the eye of the head bandit";
+                        redLabel.Text = "continue";
+                        blueLabel.Text = "";
+                        break;
+                    case 2:
+                        outputLabel.Text = "bandits attack your farm and shoot papa and all the cows" +
+                             "You notice a scar the eye of the head bandits";
+                        redLabel.Text = "continue";
+                        blueLabel.Text = "";
+                        break;
+                    case 3:
+                        outputLabel.Text = "9 years later";
+                        redLabel.Text = "continue";
+                        blueLabel.Text = "";
+                        break;
+                    case 4:
+                        outputLabel.Text = "You are sitting in a bar drowning your sorrows" +
+                            "when you overhear some guys talking about a crew a bandits named the bad boys." +
+                            "You hear one of them mention that the leader of this gang's name has a scar" +
+                            "over his eye. 'they call him Scar Face Magee' one of the men said";
+                        redLabel.Text = "Be kind and ask";
+                        blueLabel.Text = "intimidate";
+
+                        break;
+                    case 5:
+                        outputLabel.Text = "They tell you to frig off and you drink until you get" +
+                              " alcohol posioning";
+                        redLabel.Text = "Retart";
+                        blueLabel.Text = "Exit";
+                        break;
+                    case 6:
+                        outputLabel.Text = "They tell you about a group called the Bad Boys with a" +
+                            " leader they hear has a scar on his eye";
+
+                        redLabel.Text = "continue";
+                        blueLabel.Text = "";
+                        break;
+                    case 7:
+                        outputLabel.Text = "an ol man smoking his pipe in the corner looks at you" +
+                            "and fixes his hat 'The bad boys burned down my farm... I want" +
+                            "nothin more than to take them down";
+                        redLabel.Text = "open up to him";
+                        blueLabel.Text = "stay to yourself and ask where the hideout is";
+                        break;
+                    case 8:
+                        outputLabel.Text = "you too? the man says as he sits up in his chair, " +
+                            "I know where they're hideout is and could tell you where it is as long" +
+                            " as I can come with you too get em'";
+                        redLabel.Text = "sure";
+                        blueLabel.Text = "Nope";
+                        break;
+                    case 9:
+                        outputLabel.Text = "";
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+         
+        }
+
+    }
+
